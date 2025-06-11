e96226326f36f0399876d80349c79a74d4b9d9de
Aziz Seidametov <pathfinder.r51offroad@gmail.com>
2025-06-11 15:53:48 +0200

    initial commit

---
+++ src/main/java/de/ait/javalessons/springbootgr61e/SpringBootGr61EApplication.java
@@ -0,0 +1,13 @@
+package de.ait.javalessons.springbootgr61e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGr61EApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGr61EApplication.class, args);
+    }
+
+}
