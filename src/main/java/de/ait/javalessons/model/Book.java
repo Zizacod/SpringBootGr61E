234939e992bfa6233be9def66ae4aa20b9d4f9a2
Aziz Seidametov <pathfinder.r51offroad@gmail.com>
2025-06-17 14:23:58 +0200

    HomeWork Lesson_6

--- src/main/java/de/ait/javalessons/model/Book.java
+++ src/main/java/de/ait/javalessons/model/Book.java
@@ -4,7 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 
 @Data
 @Entity
@@ -23,59 +34,6 @@ public class Book {
     @Min(value = 1)
     private int publishYear;
 
-
-    public Book(String id, String title, String author, int publishYear) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.publishYear = publishYear;
-    }
-
-    public Book() {
-
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getPublishYear() {
-        return publishYear;
-    }
-
-    public void setPublishYear(int year) {
-        this.publishYear = year;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", year=" + publishYear +
-                '}';
-    }
 }
 
 
--- src/main/java/de/ait/javalessons/model/Car.java
+++ src/main/java/de/ait/javalessons/model/Car.java
@@ -1,35 +1,38 @@
 package de.ait.javalessons.model;
 
-import java.util.UUID;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
 public class Car {
 
+    @Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    @NotBlank(message = "Id не должно быть пустым")
     private String id;
 
+    @NotBlank(message = "Название не должно быть пустым")
     private  String name;
 
-    public Car() {
-    }
 
-    public Car(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public Car(String name){
-        this.id = UUID.randomUUID().toString();
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+        
         this.name = name;
     }
 }
